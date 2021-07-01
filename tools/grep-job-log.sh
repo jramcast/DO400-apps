984eab5595112b6c6b6db0ffe93e5cfd65224975
Jaime Ramirez <jaime.ramirez@redhat.com>
2021-07-01 12:33:00 +0200

    fix
    fix

--- tools/grep-job-log.sh
+++ tools/grep-job-log.sh
@@ -2,4 +2,5 @@
 oc wait --for=condition=ContainersReady \
   --timeout=60s pod --selector job-name=${1} -n ${2}
 
+oc logs --selector=job-name=${1} -n ${2} --tail=-1
 oc logs --selector=job-name=${1} -n ${2} --tail=-1 | grep "${3}"
